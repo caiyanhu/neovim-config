7d3beeff9310758a8bb6a5f16ab67f2afe57c8c5
caiyanhu <caiyanhu@cmss.chinamobile.com>
2022-02-07 17:32:01 +0800

    nvim滚动插件去掉G和gg命令，因为这俩命令在visual模式下有问题

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ require('languageServerConfig')
 
 -- Init smooth scrolling
 require('neoscroll').setup({
-  mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb', 'G', 'gg'},
+  mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
 })
 
 vim.cmd [[
