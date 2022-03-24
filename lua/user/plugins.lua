b7f94a39dc892f706c8d4ec0f6809bacf5159cda
caiyanhu <caiyanhu@cmss.chinamobile.com>
2022-03-24 09:32:46 +0800

    feat: add emmet plugin
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -79,6 +79,9 @@ return packer.startup(function(use)
 	use("lewis6991/gitsigns.nvim")
 	use("f-person/git-blame.nvim")
 
+	-- Emmet
+	use("mattn/emmet-vim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
