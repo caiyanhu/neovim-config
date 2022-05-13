5c34c7b05d58c9b39006de685e46306365ad33fc
caiyanhu <caiyanhu@cmss.chinamobile.com>
2022-05-13 16:48:22 +0800

    feat: change init.lua to fit vscode-neovim
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- init.lua
+++ init.lua
@@ -1,18 +1,28 @@
+-- global settings
 require("user.options")
 require("user.keymaps")
-require("user.plugins")
-require("user.colorscheme")
-require("user.cmp")
-require("user.lsp")
-require("user.treesitter")
-require("user.autopairs")
-require("user.comment")
-require("user.gitsigns")
-require("user.nvim-tree")
-require("user.lsp.null-ls")
-require("user.lualine")
-require("user.impatient")
-require("user.indentline")
-require("user.utils")
-require("user.floaterm")
-require("user.hop")
+
+if (vim.g.vscode) then
+  -- vscode extension
+  -- map keyboard quickfix
+  vim.api.nvim_set_keymap("n", "z=", "<cmd>call VSCodeNotify('keyboard-quickfix.openQuickFix')<Cr>", { noremap = true, silent = true })
+
+else
+  -- ordinary neovim
+  require("user.plugins")
+  require("user.colorscheme")
+  require("user.cmp")
+  require("user.lsp")
+  require("user.treesitter")
+  require("user.autopairs")
+  require("user.comment")
+  require("user.gitsigns")
+  require("user.nvim-tree")
+  require("user.lsp.null-ls")
+  require("user.lualine")
+  require("user.impatient")
+  require("user.indentline")
+  require("user.utils")
+  require("user.floaterm")
+  require("user.hop")
+end
