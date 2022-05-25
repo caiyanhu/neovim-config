b53effaf11ec3169388e7a999e734a34e4186d2a
caiyanhu <caiyanhu@cmss.chinamobile.com>
2022-05-25 17:59:30 +0800

    feat: change colorscheme
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,16 @@
 vim.cmd([[
 try
-  colorscheme tokyonight
+  colorscheme spaceduck
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
+]])
+
+vim.cmd([[
+  if exists('+termguicolors')
+    let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+    let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+    set termguicolors
+  endif
 ]])
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,103 +3,105 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
 packer.init({
-  display = {
-    open_fn = function()
-      return require("packer.util").float({ border = "rounded" })
-    end,
-  },
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
 })
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use("wbthomason/packer.nvim") -- Have packer manage itself
-  use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-  use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-  use("numToStr/Comment.nvim") -- Easily comment stuff
-  use("p00f/nvim-ts-rainbow") -- Rainbow parentheses for neovim using tree-sitter
-  use("kyazdani42/nvim-web-devicons")
-  use("kyazdani42/nvim-tree.lua")
-  use("nvim-lualine/lualine.nvim")
-  use("lewis6991/impatient.nvim")
-  use("lukas-reineke/indent-blankline.nvim")
+	-- My plugins here
+	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+	use("numToStr/Comment.nvim") -- Easily comment stuff
+	use("p00f/nvim-ts-rainbow") -- Rainbow parentheses for neovim using tree-sitter
+	use("kyazdani42/nvim-web-devicons")
+	use("kyazdani42/nvim-tree.lua")
+	use("nvim-lualine/lualine.nvim")
+	use("lewis6991/impatient.nvim")
+	use("lukas-reineke/indent-blankline.nvim")
 
-  -- Colorschemes
-  use("folke/tokyonight.nvim")
+	-- Colorschemes
+	use("folke/tokyonight.nvim")
+	use("EdenEast/nightfox.nvim")
+	use({ "pineapplegiant/spaceduck", branch = "main" })
 
-  -- cmp plugins
-  use("hrsh7th/nvim-cmp") -- The completion plugin
-  use("hrsh7th/cmp-buffer") -- buffer completions
-  use("hrsh7th/cmp-path") -- path completions
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-nvim-lua")
-  use("onsails/lspkind.nvim") -- icons on the completion popups
+	-- cmp plugins
+	use("hrsh7th/nvim-cmp") -- The completion plugin
+	use("hrsh7th/cmp-buffer") -- buffer completions
+	use("hrsh7th/cmp-path") -- path completions
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
+	use("onsails/lspkind.nvim") -- icons on the completion popups
 
-  -- snippets
-  use("L3MON4D3/LuaSnip") -- snippet engine
-  use("saadparwaiz1/cmp_luasnip") -- snippet source
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- snippet source
 
-  -- LSP
-  use("neovim/nvim-lspconfig") -- enable LSP
+	-- LSP
+	use("neovim/nvim-lspconfig") -- enable LSP
 
-  -- formatters and linters
-  use("dense-analysis/ale")
+	-- formatters and linters
+	use("dense-analysis/ale")
 
-  -- Telescope
-  use("nvim-telescope/telescope.nvim")
-  use("nvim-telescope/telescope-media-files.nvim")
+	-- Telescope
+	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope-media-files.nvim")
 
-  -- Treesitter
-  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	-- Treesitter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
-  -- Auto Pairs
-  use("windwp/nvim-autopairs")
+	-- Auto Pairs
+	use("windwp/nvim-autopairs")
 
-  -- Git
-  use("lewis6991/gitsigns.nvim")
-  use("sindrets/diffview.nvim")
+	-- Git
+	use("lewis6991/gitsigns.nvim")
+	use("sindrets/diffview.nvim")
 
-  -- Emmet
-  use("mattn/emmet-vim")
+	-- Emmet
+	use("mattn/emmet-vim")
 
-  -- Float Terminal
-  use("voldikss/vim-floaterm")
+	-- Float Terminal
+	use("voldikss/vim-floaterm")
 
-  -- Motions
-  use("phaazon/hop.nvim")
+	-- Motions
+	use("phaazon/hop.nvim")
 
-  -- JSON Schema Store
-  use("b0o/schemastore.nvim")
+	-- JSON Schema Store
+	use("b0o/schemastore.nvim")
 
-  -- todos
-  use("folke/todo-comments.nvim")
+	-- todos
+	use("folke/todo-comments.nvim")
 
-  -- preview color
-  use("norcalli/nvim-colorizer.lua")
+	-- preview color
+	use("norcalli/nvim-colorizer.lua")
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
