72ba6f27cc3166fd5e09aebd08f2263061fd91d9
caiyanhu <caiyanhu@cmss.chinamobile.com>
2022-06-01 11:46:16 +0800

    feat: change edit sign in winbar
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/winbar.lua
+++ lua/user/winbar.lua
@@ -5,7 +5,7 @@ vim.api.nvim_set_hl(0, "WinBarModified", { bg = "#dedede", fg = "#363636" })
 
 function M.eval()
 	local file_path = vim.api.nvim_eval_statusline("%f", {}).str
-	local modified_status = vim.api.nvim_eval_statusline("%M", {}).str == "+" and "🐯" or ""
+	local modified_status = vim.api.nvim_eval_statusline("%M", {}).str == "+" and "✍️" or ""
 
 	file_path = file_path:gsub("/", "➤")
 
