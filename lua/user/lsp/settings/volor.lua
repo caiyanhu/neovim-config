56b63b9fefb0f726482e0e26782ebb781d3c4231
caiyanhu <caiyanhu@gmail.com>
2022-07-01 15:32:16 +0800

    feat: change volor ts path by platform
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/lsp/settings/volor.lua
+++ lua/user/lsp/settings/volor.lua
@@ -1,7 +1,15 @@
+local os_name = vim.loop.os_uname().sysname
+local path = ""
+if os_name == "Darwin" then
+	path = "/usr/local/lib"
+elseif os_name == "Linux" then
+	path = "/usr/lib"
+end
+
 return {
 	init_options = {
 		typescript = {
-			serverPath = "/usr/local/lib/node_modules/typescript/lib/tsserverlibrary.js", -- Alternative location if installed as root:
+			serverPath = path .. "/node_modules/typescript/lib/tsserverlibrary.js", -- Alternative location if installed as root:
 		},
 	},
 }
