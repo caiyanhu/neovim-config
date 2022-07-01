73bca4d2313ebf08c4892dd155c74323b0b7ece8
caiyanhu <caiyanhu@gmail.com>
2022-07-01 15:03:14 +0800

    feat: move impatient.nvim to the top of init.lua
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- init.lua
+++ init.lua
@@ -1,33 +1,19 @@
--- global settings
+require("user.impatient")
 require("user.options")
 require("user.keymaps")
-
-if vim.g.vscode then
-	-- vscode extension
-	-- map keyboard quickfix
-	vim.api.nvim_set_keymap(
-		"n",
-		"z=",
-		"<cmd>call VSCodeNotify('keyboard-quickfix.openQuickFix')<Cr>",
-		{ noremap = true, silent = true }
-	)
-else
-	-- ordinary neovim
-	require("user.plugins")
-	require("user.colorscheme")
-	require("user.cmp")
-	require("user.lsp")
-	require("user.ale")
-	require("user.treesitter")
-	require("user.autopairs")
-	require("user.comment")
-	require("user.gitsigns")
-	require("user.nvim-tree")
-	require("user.lualine")
-	require("user.impatient")
-	require("user.indentline")
-	require("user.utils")
-	require("user.floaterm")
-	require("user.colorizer")
-	require("user.snippets")
-end
+require("user.plugins")
+require("user.colorscheme")
+require("user.cmp")
+require("user.lsp")
+require("user.ale")
+require("user.treesitter")
+require("user.autopairs")
+require("user.comment")
+require("user.gitsigns")
+require("user.nvim-tree")
+require("user.lualine")
+require("user.indentline")
+require("user.utils")
+require("user.floaterm")
+require("user.colorizer")
+require("user.snippets")
