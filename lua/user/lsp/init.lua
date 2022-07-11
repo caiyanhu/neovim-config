58f77cd44e7c64f58c862f02f52f43ef56bce192
caiyanhu <caiyanhu@cmss.chinamobile.com>
2022-07-11 21:51:35 +0800

    feat: add taiwindcss lsp
    Code Source From: Self Code|OpenSource|3rd party Code|Other
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,44 +1,54 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-  return
+	return
 end
 
 local M = {}
 
 local function on_attach(client, bufnr)
-  -- Configure key mappings
-  require("user.lsp.keymaps").setup(client, bufnr)
+	-- Configure key mappings
+	require("user.lsp.keymaps").setup(client, bufnr)
 
-  -- Configure highlighting
-  require("user.lsp.highlighting").setup(client)
+	-- Configure highlighting
+	require("user.lsp.highlighting").setup(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not has_cmp then
-  return
+	return
 end
 
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 local opts = {
-  on_attach = on_attach,
-  capabilities = M.capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  },
+	on_attach = on_attach,
+	capabilities = M.capabilities,
+	flags = {
+		debounce_text_changes = 150,
+	},
 }
 require("user.lsp.handlers").setup()
 
-local servers = { "tsserver", "volar", "html", "cssls", "jsonls", "rust_analyzer", "volar", "sumneko_lua" }
+local servers = {
+	"tsserver",
+	"volar",
+	"html",
+	"cssls",
+	"jsonls",
+	"rust_analyzer",
+	"volar",
+	"sumneko_lua",
+	"tailwindcss",
+}
 for _, lsp in pairs(servers) do
-  if lsp == "sumneko_lua" then
-    local lua_opts = require("user.lsp.settings.sumneko_lua")
-    opts = vim.tbl_deep_extend("force", lua_opts, opts)
-  end
-  if lsp == "volar" then
-    local lua_opts = require("user.lsp.settings.volor")
-    opts = vim.tbl_deep_extend("force", lua_opts, opts)
-  end
-  require("lspconfig")[lsp].setup(opts)
+	if lsp == "sumneko_lua" then
+		local lua_opts = require("user.lsp.settings.sumneko_lua")
+		opts = vim.tbl_deep_extend("force", lua_opts, opts)
+	end
+	if lsp == "volar" then
+		local lua_opts = require("user.lsp.settings.volor")
+		opts = vim.tbl_deep_extend("force", lua_opts, opts)
+	end
+	require("lspconfig")[lsp].setup(opts)
 end
