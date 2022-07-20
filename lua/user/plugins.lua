113f4a0225b095a260df2069465e5a9c1699ed29
caiyanhu <caiyanhu@cmss.chinamobile.com>
2022-07-20 22:43:38 +0800

    feat: add lsp-installer to manage servers
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -58,6 +58,7 @@ return packer.startup(function(use)
 
 	-- LSP
 	use("neovim/nvim-lspconfig") -- enable LSP
+  use("williamboman/nvim-lsp-installer")
 
 	-- formatters and linters
 	use("dense-analysis/ale")
