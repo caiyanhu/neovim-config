4f8012e97d4e63f55ab602bebaf99658a3223f73
caiyanhu <caiyanhu@gmail.com>
2022-08-31 19:20:26 +0800

    feat: add snippets
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/snippets.lua
+++ lua/user/snippets.lua
@@ -6,43 +6,97 @@ end
 local snip = ls.snippet
 local text = ls.text_node
 local insert = ls.insert_node
-local func = ls.function_node
 
-local date = function()
-	return { os.date("%Y-%m-%d") }
-end
+local JS_Snip = {
+	snip("clg", {
+		text("console.log("),
+		insert(0),
+		text(")"),
+	}),
+	snip("edf", {
+		text("export default "),
+		insert(0),
+	}),
+	snip("cd", {
+		text("console.dir("),
+		insert(0),
+		text(")"),
+	}),
+	snip("ce", {
+		text("console.error("),
+		insert(0),
+		text(")"),
+	}),
+	snip("de", {
+		text("debugger;"),
+	}),
+	snip("clg", {
+		text("console.log("),
+		insert(0),
+		text(")"),
+	}),
+}
+
+local LUA_Snip = {
+	snip("shebang", {
+		text({ "#!/usr/bin/lua", "", "" }),
+		insert(0),
+	}),
+	snip("req", {
+		text("require('"),
+		insert(1, "Module-name"),
+		text("')"),
+		insert(0),
+	}),
+	snip("func", {
+		text("function("),
+		insert(1, "Arguments"),
+		text({ ")", "\t" }),
+		insert(2),
+		text({ "", "end", "" }),
+		insert(0),
+	}),
+	snip("forp", {
+		text("for "),
+		insert(1, "k"),
+		text(", "),
+		insert(2, "v"),
+		text(" in pairs("),
+		insert(3, "table"),
+		text({ ") do", "\t" }),
+		insert(4),
+		text({ "", "end", "" }),
+		insert(0),
+	}),
+	snip("fori", {
+		text("for "),
+		insert(1, "k"),
+		text(", "),
+		insert(2, "v"),
+		text(" in ipairs("),
+		insert(3, "table"),
+		text({ ") do", "\t" }),
+		insert(4),
+		text({ "", "end", "" }),
+		insert(0),
+	}),
+	snip("if", {
+		text("if "),
+		insert(1),
+		text({ " then", "\t" }),
+		insert(2),
+		text({ "", "end", "" }),
+		insert(0),
+	}),
+	snip("M", {
+		text({ "local M = {}", "", "" }),
+		insert(0),
+		text({ "", "", "return M" }),
+	}),
+}
 
 ls.add_snippets(nil, {
-	all = {
-		snip({
-			trig = "date",
-			namr = "Date",
-			dscr = "Date in the form of YYYY-MM-DD",
-		}, {
-			func(date, {}),
-		}),
-	},
-	typescript = {
-		snip("clg", {
-			text("console.log("),
-			insert(0),
-			text(")"),
-		}),
-		snip("trycatch", {
-			text("try {"),
-			text("\t"),
-			insert(0),
-			text("}catch(error){"),
-			text("\t"),
-			insert(1),
-			text("}"),
-		}),
-	},
-	javascript = {
-		snip("clg", {
-			text("console.log("),
-			insert(0),
-			text(")"),
-		}),
-	},
+	lua = LUA_Snip,
+	typescript = JS_Snip,
+	javascript = JS_Snip,
 })
