6191f8777ed5dc4e597bddf7b5ed13c39e2668de
caiyanhu <caiyanhu@gmail.com>
2022-08-31 17:29:41 +0800

    feat: change bookmarks sign
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/snippets.lua
+++ lua/user/snippets.lua
@@ -4,12 +4,9 @@ if not luasnip_status_ok then
 end
 -- some shorthands...
 local snip = ls.snippet
-local node = ls.snippet_node
 local text = ls.text_node
 local insert = ls.insert_node
 local func = ls.function_node
-local choice = ls.choice_node
-local dynamicn = ls.dynamic_node
 
 local date = function()
 	return { os.date("%Y-%m-%d") }
