d78f86ab1ee949cd22a7d2f36fbbd750fb7e1455
caiyanhu <caiyanhu@cmss.chinamobile.com>
2022-10-30 21:07:09 +0800

    feat: xxxx
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

---
+++ after/plugin/treesitter.rc.lua
@@ -0,0 +1,42 @@
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+ts.setup({
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	autotag = {
+		enable = true,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+	},
+	autopairs = {
+		enable = true,
+	},
+	ensure_installed = {
+		"tsx",
+		"typescript",
+		"javascript",
+		"json",
+		"html",
+		"scss",
+		"lua",
+		"css",
+		"vue",
+		"rust",
+		"toml",
+	},
+})
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
