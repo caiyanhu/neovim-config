82fcc0e1675dd53ab8c49aede102c5356001faa9
caiyanhu <caiyanhu@cmss.chinamobile.com>
2023-01-18 16:35:45 +0800

    format: 对所有文件做了格式化
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,41 +1,41 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
 if not status then
-  return
+	return
 end
 
 ts.setup({
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  autotag = {
-    enable = true,
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-  },
-  autopairs = {
-    enable = true,
-  },
-  ensure_installed = {
-    "tsx",
-    "typescript",
-    "javascript",
-    "json",
-    "html",
-    "scss",
-    "lua",
-    "css",
-    "vue",
-    "rust",
-    "toml",
-  },
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	autotag = {
+		enable = true,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+	},
+	autopairs = {
+		enable = true,
+	},
+	ensure_installed = {
+		"tsx",
+		"typescript",
+		"javascript",
+		"json",
+		"html",
+		"scss",
+		"lua",
+		"css",
+		"vue",
+		"rust",
+		"toml",
+	},
 })
 
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
