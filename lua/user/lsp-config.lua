b94fc69779f81fc96900d4cb677c217639726b98
caiyanhu <caiyanhu@cmss.chinamobile.com>
2023-04-26 15:31:40 +0800

    feat: 修改init.lua, 适配vscode
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/lsp-config.lua
+++ lua/user/lsp-config.lua
@@ -1,14 +1,14 @@
 local status, lsp_config = pcall(require, "lspconfig")
 if not status then
-  return
+	return
 end
 local status_lsp_source, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_lsp_source then
-  return
+	return
 end
 local has_scheme_store, schemaStore = pcall(require, "schemastore")
 if not has_scheme_store then
-  return
+	return
 end
 
 -- Add additional capabilities supported by nvim-cmp
@@ -16,75 +16,75 @@ local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- lua的配置
 local lua_ls_opts = {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = "Lua 5.4",
-        path = {
-          "?/init.lua",
-        },
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false, -- stop lua_ls asks about working environment on every startup
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "Lua 5.4",
+				path = {
+					"?/init.lua",
+				},
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false, -- stop lua_ls asks about working environment on every startup
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 }
 
 -- volar的配置
 local os_name = vim.loop.os_uname().sysname
 local path = ""
 if os_name == "Darwin" then
-  path = "/usr/local/lib"
+	path = "/usr/local/lib"
 elseif os_name == "Linux" then
-  -- path = "/usr/lib"
-  path = "~/.local/share/nvm/v16.19.1/lib" -- 通过nvm全局安装的,路径在home目录下
+	-- path = "/usr/lib"
+	path = "~/.local/share/nvm/v16.19.1/lib" -- 通过nvm全局安装的,路径在home目录下
 end
 local volar_opts = {
-  init_options = {
-    typescript = {
-      tsdk = path .. "/node_modules/typescript/lib", -- Alternative location if installed as root:
-    },
-  },
+	init_options = {
+		typescript = {
+			tsdk = path .. "/node_modules/typescript/lib", -- Alternative location if installed as root:
+		},
+	},
 }
 
 -- jsonls的配置
 local jsonls_opts = {
-  settings = {
-    json = {
-      schemas = schemaStore.json.schemas(),
-      validate = { enable = true },
-    },
-  },
+	settings = {
+		json = {
+			schemas = schemaStore.json.schemas(),
+			validate = { enable = true },
+		},
+	},
 }
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { "lua_ls", "tsserver", "volar", "jsonls", "html", "cssls", "rust_analyzer" }
+local servers = { "lua_ls", "tsserver", "volar", "jsonls", "html", "cssls", "rust_analyzer", "taplo" }
 -- jsonls html cssls在archlinux下均通过pacman安装 https://aur.archlinux.org/packages/vscode-langservers-extracted
 -- rust_analyzer是通过rustup component add rust-src安装
 for _, lsp in ipairs(servers) do
-  local opts = {
-    capabilities = capabilities,
-  }
-  if lsp == "lua_ls" then
-    opts = vim.tbl_deep_extend("force", lua_ls_opts, opts)
-  end
-  if lsp == "volar" then
-    opts = vim.tbl_deep_extend("force", volar_opts, opts)
-  end
-  if lsp == "jsonls" then
-    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-  end
-  lsp_config[lsp].setup(opts)
+	local opts = {
+		capabilities = capabilities,
+	}
+	if lsp == "lua_ls" then
+		opts = vim.tbl_deep_extend("force", lua_ls_opts, opts)
+	end
+	if lsp == "volar" then
+		opts = vim.tbl_deep_extend("force", volar_opts, opts)
+	end
+	if lsp == "jsonls" then
+		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+	end
+	lsp_config[lsp].setup(opts)
 end
