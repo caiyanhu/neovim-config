d552ab3a0121eda97fda87071dcbfe16a8b5d0e6
caiyanhu <caiyanhu@cmss.chinamobile.com>
2023-07-26 20:30:30 +0800

    feat: add ts snippets
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/snippets.lua
+++ lua/user/snippets.lua
@@ -1,2 +1,9 @@
 -- load friendly-snippets
 require("luasnip.loaders.from_vscode").lazy_load()
+
+local status, luaSnip = pcall(require, "luasnip")
+if not status then
+  return
+end
+
+luaSnip.filetype_extend("typescript", { "javascript" })
