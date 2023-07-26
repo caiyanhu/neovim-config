add606a8c8ca6a5312f5576339599b030a436e03
caiyanhu <caiyanhu@cmss.chinamobile.com>
2023-07-26 14:44:36 +0800

    feat: add lua_ls configure
    remove "Do you need to configure your work environment as luv"
    
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -35,6 +35,7 @@ local servers = {
 				workspace = {
 					-- Make the server aware of Neovim runtime files
 					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
 				},
 				-- Do not send telemetry data containing a randomized but unique identifier
 				telemetry = {
