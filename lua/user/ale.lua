62adaaef6c4473ecd8324b6abe38e99d5db2238c
caiyanhu <caiyanhu@cmss.chinamobile.com>
2023-08-11 16:37:15 +0800

    feat: 升级lua_ls,删除stylua,使用内置的格式化工具
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/user/ale.lua
+++ lua/user/ale.lua
@@ -3,32 +3,32 @@ vim.g.ale_linters_explicit = 1
 
 -- linters
 vim.g.ale_linters = {
-	css = { "stylelint", "vscode-css-language-server", "cspell" },
-	html = { "cspell" },
-	javascript = { "tsserver", "eslint", "cspell" },
-	javascriptreact = { "stylint", "vscode-css-language-server", "eslint", "cspell" },
-	json = { "vscode-json-language-server", "cspell" },
-	lua = { "selene", "cspell" },
-	rust = { "analyzer", "cspell" },
-	typescript = { "tsserver", "eslint", "cspell" },
-	typescriptreact = { "stylint", "vscode-css-language-server", "eslint", "cspell" },
-	vue = { "volar", "cspell" },
+  css = { "stylelint", "vscode-css-language-server", "cspell" },
+  html = { "cspell" },
+  javascript = { "tsserver", "eslint", "cspell" },
+  javascriptreact = { "stylint", "vscode-css-language-server", "eslint", "cspell" },
+  json = { "vscode-json-language-server", "cspell" },
+  lua = { "selene", "cspell" },
+  rust = { "analyzer", "cspell" },
+  typescript = { "tsserver", "eslint", "cspell" },
+  typescriptreact = { "stylint", "vscode-css-language-server", "eslint", "cspell" },
+  vue = { "volar", "cspell" },
 }
 
 -- fixers
 vim.g.ale_fixers = {
-	css = { "prettier" },
-	html = { "prettier" },
-	javascript = { "prettier" },
-	javascriptreact = { "prettier" },
-	json = { "prettier" },
-	less = { "prettier" },
-	lua = { "stylua" },
-	rust = { "rustfmt" },
-	typescript = { "prettier" },
-	typescriptreact = { "prettier" },
-	vue = { "prettier" },
-	yaml = { "yamlfix" },
+  css = { "prettier" },
+  html = { "prettier" },
+  javascript = { "prettier" },
+  javascriptreact = { "prettier" },
+  json = { "prettier" },
+  less = { "prettier" },
+  -- lua = { "stylua" },
+  rust = { "rustfmt" },
+  typescript = { "prettier" },
+  typescriptreact = { "prettier" },
+  vue = { "prettier" },
+  yaml = { "yamlfix" },
 }
 
 vim.g.ale_fix_on_save = 1
