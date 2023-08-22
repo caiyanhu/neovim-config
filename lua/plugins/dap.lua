dd87d4e2b630e417aa47a53e776ccd70979a3780
caiyanhu <caiyanhu@cmss.chinamobile.com>
2023-08-22 18:37:58 +0800

    tweak dap config
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,56 +2,71 @@ return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
-		"theHamsta/nvim-dap-virtual-text",
-		"nvim-telescope/telescope-dap.nvim",
+		{
+			"mxsdev/nvim-dap-vscode-js",
+			dependencies = {
+				"microsoft/vscode-js-debug",
+				build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+			},
+		},
 	},
 	config = function()
-		vim.keymap.set("n", "<F5>", function()
-			require("telescope").extensions.dap.configurations({})
-		end)
-		vim.keymap.set("n", "<F10>", function()
-			require("dap").step_over()
-		end)
-		vim.keymap.set("n", "<F11>", function()
-			require("dap").step_into()
-		end)
-		vim.keymap.set("n", "<F12>", function()
-			require("dap").step_out()
-		end)
-		vim.keymap.set("n", "<Leader>db", function()
-			require("dap").toggle_breakpoint()
-		end)
-		vim.keymap.set("n", "<Leader>dr", function()
-			require("dap").repl.open()
-		end)
-		vim.keymap.set("n", "<Leader>dl", function()
-			require("dap").run_last()
-		end)
-		vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
-			require("dap.ui.widgets").hover()
-		end)
-		vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
-			require("dap.ui.widgets").preview()
-		end)
-		vim.keymap.set("n", "<Leader>df", function()
-			local widgets = require("dap.ui.widgets")
-			widgets.centered_float(widgets.frames)
-		end)
-		vim.keymap.set("n", "<Leader>ds", function()
-			local widgets = require("dap.ui.widgets")
-			widgets.centered_float(widgets.scopes)
-		end)
-		local dap, dapui = require("dap"), require("dapui")
-		require("nvim-dap-virtual-text").setup({})
-		require("dapui").setup()
-		dap.listeners.after.event_initialized["dapui_config"] = function()
-			dapui.open()
+		require("dap-vscode-js").setup({
+			debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+			adapters = {
+				"chrome",
+				"pwa-node",
+				"pwa-chrome",
+				"pwa-msedge",
+				"node-terminal",
+				"pwa-extensionHost",
+				"node",
+			},
+		})
+
+		local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+		for _, language in ipairs(js_based_languages) do
+			require("dap").configurations[language] = {
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Launch file",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "pwa-node",
+					request = "attach",
+					name = "Attach",
+					processId = require("dap.utils").pick_process,
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "pwa-chrome",
+					request = "launch",
+					name = 'Start Chrome with "localhost"',
+					url = "http://localhost:3000",
+					webRoot = "${workspaceFolder}",
+					userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+				},
+			}
 		end
-		dap.listeners.before.event_terminated["dapui_config"] = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited["dapui_config"] = function()
-			dapui.close()
-		end
+
+		local dap, dap_ui = require("dap"), require("dapui")
+		-- DAP ui
+		dap_ui.setup()
+
+		-- keymaps
+		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+		vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
+		vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
+		vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
+		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+		vim.keymap.set("n", "<F7>", dap_ui.toggle, { desc = "Debug: See last session result." })
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+
+		dap.listeners.after.event_initialized["dapui_config"] = dap_ui.open
+		dap.listeners.before.event_terminated["dapui_config"] = dap_ui.close
+		dap.listeners.before.event_exited["dapui_config"] = dap_ui.close
 	end,
 }
