4a28e6cce20415261349a18ca7234cc41aeca78c
caiyanhu <caiyanhu@cmss.chinamobile.com>
2023-09-26 17:11:32 +0800

    feat: remove telescope_live_grep_args plugin
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,6 @@ return {
 				build = jit.os == "OSX" and "make"
 					or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 			},
-			"nvim-telescope/telescope-live-grep-args.nvim",
 		},
 		keys = {
 			{ "<leader>?", "<cmd>lua require('telescope.builtin').oldfiles()<cr>" },
@@ -25,11 +24,9 @@ return {
 				end,
 			},
 			{ "<C-p>", "<cmd>lua require('telescope.builtin').find_files()<cr>" },
-			{ "<leader>ff", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>" },
-			{ "<leader>gc", "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor()<cr>" },
+			{ "<leader>ff", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
 		},
 		config = function()
-			local lga_actions = require("telescope-live-grep-args.actions")
 			require("telescope").setup({
 				extensions = {
 					fzf = {
@@ -38,19 +35,9 @@ return {
 						override_file_sorter = true,
 						case_mode = "smart_case",
 					},
-					live_grep_args = {
-						auto_quoting = true, -- enable/disable auto-quoting
-						mappings = {
-							i = {
-								["<C-k>"] = lga_actions.quote_prompt(),
-								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-							},
-						},
-					},
 				},
 			})
 			require("telescope").load_extension("fzf")
-			require("telescope").load_extension("live_grep_args")
 		end,
 	},
 }
