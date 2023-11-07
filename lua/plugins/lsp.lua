57e05cdc24437f33f6d1dc36b26de6b9b38a15af
caiyanhu <caiyanhu@cmss.chinamobile.com>
2023-11-07 15:10:20 +0800

    feat: 增加折叠功能
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -62,6 +62,10 @@ return {
       require("mason").setup()
 
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
       require("mason-lspconfig").setup({
         ensure_installed = vim.tbl_keys(servers),
         handlers = {
