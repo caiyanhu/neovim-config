ea869aa46ac612a8d60915126778f2b52eb6b097
caiyanhu <caiyanhu@cmss.chinamobile.com>
2024-04-24 18:01:09 +0800

    feat: 配置luasnip插件
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,41 +8,20 @@ return {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-cmdline',
-      {
-        'saadparwaiz1/cmp_luasnip',
-        dependencies = {
-          'L3MON4D3/LuaSnip',
-          dependencies = {
-            'rafamadriz/friendly-snippets',
-          },
-        },
-      },
+      'saadparwaiz1/cmp_luasnip',
     },
     config = function()
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-          and vim.api
-              .nvim_buf_get_lines(0, line - 1, line, true)[1]
-              :sub(col, col)
-              :match '%s'
-            == nil
-      end
-
-      require('luasnip.loaders.from_vscode').lazy_load()
       local luasnip = require 'luasnip'
+
       local cmp = require 'cmp'
       local cmp_mappings = {
         ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- they way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
+          elseif luasnip.jumpable(1) then
+            luasnip.jump(1)
+          elseif luasnip.expandable() then
+            luasnip.expand()
           else
             fallback()
           end
@@ -72,7 +51,6 @@ return {
           { name = 'luasnip' },
           { name = 'path' },
           { name = 'buffer' },
-          { name = 'codeium' },
           { name = 'crates' },
         },
       }
