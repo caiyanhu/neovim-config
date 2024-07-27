a523a8434eed4a67bb9c7e9f63c335666602a405
caiyanhu <caiyanhu@cmss.chinamobile.com>
2024-07-27 18:52:34 +0800

    feat: add format config
    Code Source From: Self Code
    Description:
    Jira: #CLOUDDB-48
    市场项目编号（名称）:

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -30,11 +30,6 @@ return {
 
       require('conform').setup {
         formatters_by_ft = formatters,
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_fallback = true,
-          async = false,
-        },
       }
     end,
   },
