86b345a2face586eee782d29882bfaea0516fde9
caiyanhu <caiyanhu@cmss.chinamobile.com>
2024-11-22 12:29:30 +0800

    fix: 修复vue-language-server配置问题
    Code Source From: Self Code
    Description:
    Jira:
    市场项目编号（名称）:

--- lua/lsp/common.lua
+++ lua/lsp/common.lua
@@ -15,54 +15,92 @@ return {
     config = function()
       local server_names = {
         'lua_ls',
-        'volar@1.8.27',
+        'volar',
         'jsonls',
         'rust_analyzer',
         'taplo',
         'cssls',
-      }
-      local server_settings = {
-        lua_ls = {
-          Lua = {
-            workspace = {
-              checkThirdParty = false,
-            },
-            telemetry = {
-              enable = false,
-            },
-          },
-        },
-        volar = {},
-        jsonls = {
-          json = {
-            schemas = require('schemastore').json.schemas(),
-            validate = { enable = true },
-          },
-        },
-        cssls = {},
-        taplo = {},
-        rust_analyzer = {},
+        'ts_ls',
       }
 
       require('neodev').setup()
       require('fidget').setup()
       require('mason').setup()
 
-      local util = require 'lspconfig.util'
-
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       capabilities.textDocument.foldingRange = {
         dynamicRegistration = false,
         lineFoldingOnly = true,
       }
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
       require('mason-lspconfig').setup {
         ensure_installed = server_names,
         handlers = {
+          -- default handler
           function(server_name)
             require('lspconfig')[server_name].setup {
-              root_dir = util.root_pattern '.git',
-              settings = server_settings[server_name],
-              on_attach = on_attach,
+              capabilities = capabilities,
+            }
+          end,
+
+          -- Use volar for only .vue files and ts_ls for .ts and .js files. see https://github.com/vuejs/language-tools/tree/master?tab=readme-ov-file Non-Hybrid mode section
+          -- this is the custom handler for typescript
+          ts_ls = function()
+            local vue_language_server_path = require('mason-registry')
+              .get_package('vue-language-server')
+              :get_install_path() .. '/node_modules/@vue/language-server'
+
+            require('lspconfig').ts_ls.setup {
+              init_options = {
+                plugins = {
+                  {
+                    name = '@vue/typescript-plguin',
+                    location = vue_language_server_path,
+                    languages = { 'vue' },
+                  },
+                },
+              },
+              capabilities = capabilities,
+            }
+          end,
+
+          -- this is the custom handler for volar
+          volar = function()
+            require('lspconfig').volar.setup {
+              init_options = {
+                vue = {
+                  hybridMode = false,
+                },
+              },
+              capabilities = capabilities,
+            }
+          end,
+
+          -- this is the custom handler for json
+          jsonls = function()
+            require('lspconfig').jsonls.setup {
+              settings = {
+                json = {
+                  schemas = require('schemastore').json.schemas(),
+                  validate = { enable = true },
+                },
+              },
+              capabilities = capabilities,
+            }
+          end,
+
+          -- this is the custom handler for lua_ls
+          lua_ls = function()
+            require('lspconfig').lua_ls.setup {
+              Lua = {
+                workspace = {
+                  checkThirdParty = false,
+                },
+                telemetry = {
+                  enable = false,
+                },
+              },
               capabilities = capabilities,
             }
           end,
