bb657198a7a4aad9856ffd39c5bcc50d32ac32bd
caiyanhu <caiyanhu@cmss.chinamobile.com>
2025-01-15 09:07:59 +0800

    feat: add typos_lsp for spell check
    Code Source From: Self Code
    Description:
    Jira:
    市场项目编号（名称）:

--- lua/lsp/common.lua
+++ lua/lsp/common.lua
@@ -21,6 +21,7 @@ return {
         'taplo',
         'cssls',
         'ts_ls',
+        'typos_lsp',
       }
 
       require('neodev').setup()
@@ -36,6 +37,7 @@ return {
 
       require('mason-lspconfig').setup {
         ensure_installed = server_names,
+        automatic_installation = server_names,
         handlers = {
           -- default handler
           function(server_name)
@@ -102,6 +104,23 @@ return {
                 },
               },
               capabilities = capabilities,
+            }
+          end,
+
+          -- this is the custom handler for typos_lsp
+          typos_lsp = function()
+            require('lspconfig').typos_lsp.setup {
+              -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+              cmd_env = { RUST_LOG = 'error' },
+              init_options = {
+                -- Custom config. Used together with a config file found in the workspace or its parents,
+                -- taking precedence for settings declared in both.
+                -- Equivalent to the typos `--config` cli argument.
+                config = '~/code/typos-lsp/crates/typos-lsp/tests/typos.toml',
+                -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
+                -- Defaults to error.
+                diagnosticSeverity = 'Error',
+              },
             }
           end,
         },
