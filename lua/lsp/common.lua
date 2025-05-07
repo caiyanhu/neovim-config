3b8aff32cc6922a697fe3dfc6cb6633a96d3dedc
caiyanhu <caiyanhu@cmss.chinamobile.com>
2025-05-07 08:47:01 +0800

    feat: 删除typos-lsp,因为它无法将单词标记为正确的单词
    Code Source From: Self Code
    Description:
    Jira:
    市场项目编号（名称）:

--- lua/lsp/common.lua
+++ lua/lsp/common.lua
@@ -21,7 +21,7 @@ return {
         'taplo',
         'cssls',
         'ts_ls',
-        'typos_lsp',
+        -- cspell不是一个有效的lspconfig server名称，但lint时需要用到，所以需要手动安装 Mason Install cspell
       }
 
       require('neodev').setup()
@@ -104,23 +104,6 @@ return {
                 },
               },
               capabilities = capabilities,
-            }
-          end,
-
-          -- this is the custom handler for typos_lsp
-          typos_lsp = function()
-            require('lspconfig').typos_lsp.setup {
-              -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
-              cmd_env = { RUST_LOG = 'error' },
-              init_options = {
-                -- Custom config. Used together with a config file found in the workspace or its parents,
-                -- taking precedence for settings declared in both.
-                -- Equivalent to the typos `--config` cli argument.
-                config = '~/code/typos-lsp/crates/typos-lsp/tests/typos.toml',
-                -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
-                -- Defaults to error.
-                diagnosticSeverity = 'Error',
-              },
             }
           end,
         },
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,14 +2,11 @@ return {
   {
     'echasnovski/mini.comment',
     event = 'VeryLazy',
-    cond = not vim.g.vscode,
     version = false,
-    config = true,
   },
   {
     'echasnovski/mini.pairs',
     event = 'VeryLazy',
-    cond = not vim.g.vscode,
     version = false,
     config = true,
   },
