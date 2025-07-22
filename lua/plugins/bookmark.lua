71e7e0848c22e170402d7dbd41abbbab84bf8970
caiyanhu <caiyanhu@cmss.chinamobile.com>
2025-07-22 18:17:59 +0800

    feat: add grep resume
    Code Source From: Self Code
    Description:
    Jira:
    市场项目编号（名称）:

--- lua/plugins/bookmark.lua
+++ lua/plugins/bookmark.lua
@@ -1,30 +1,54 @@
 return {
   {
-    'tomasky/bookmarks.nvim',
+    'MattesGroeger/vim-bookmarks',
     event = { 'BufWritePost', 'BufReadPost' },
     cond = not vim.g.vscode,
     config = function()
-      require('bookmarks').setup {
-        -- sign_priority = 8,  --set bookmark sign priority to cover other sign
-        save_file = vim.fn.expand '$HOME/.bookmarks', -- bookmarks save file path
-        keywords = {
-          ['@t'] = '☑️ ', -- mark annotation startswith @t ,signs this icon as `Todo`
-          ['@w'] = '⚠️ ', -- mark annotation startswith @w ,signs this icon as `Warn`
-          ['@f'] = '⛏ ', -- mark annotation startswith @f ,signs this icon as `Fix`
-          ['@n'] = ' ', -- mark annotation startswith @n ,signs this icon as `Note`
-        },
-        on_attach = function(bufnr)
-          local bm = require 'bookmarks'
-          local map = vim.keymap.set
-          map('n', 'mm', bm.bookmark_toggle) -- add or remove bookmark at current line
-          map('n', 'mi', bm.bookmark_ann) -- add or edit mark annotation at current line
-          map('n', 'mc', bm.bookmark_clean) -- clean all marks in local buffer
-          map('n', 'mn', bm.bookmark_next) -- jump to next mark in local buffer
-          map('n', 'mp', bm.bookmark_prev) -- jump to previous mark in local buffer
-          map('n', 'ml', bm.bookmark_list) -- show marked file list in quickfix window
-          map('n', 'mx', bm.bookmark_clear_all) -- removes all bookmarks
-        end,
-      }
+      -- vim.g.bookmark_sign = "💙"
+      -- vim.g.bookmark_sign = "♥"
+      -- vim.g.bookmark_sign = "⚑"
+      -- vim.g.bookmark_sign = "🐯"
+      -- vim.g.bookmark_sign = "🚩"
+      -- vim.g.bookmark_sign = "🎀"
+      -- vim.g.bookmark_sign = "🔵"
+      -- vim.g.bookmark_sign = "🔥"
+      -- vim.g.bookmark_sign = "⭐"
+      vim.g.bookmark_sign = '🎯'
+      vim.g.bookmark_highlight_lines = 1
+
+      vim.api.nvim_set_keymap(
+        'n',
+        'mm',
+        ':BookmarkToggle<CR>',
+        { noremap = true, silent = true }
+      )
+      vim.api.nvim_set_keymap(
+        'n',
+        'mi',
+        ':BookmarkAnnotate',
+        { noremap = true, silent = true }
+      )
+      vim.api.nvim_set_keymap(
+        'n',
+        'mn',
+        ':BookmarkNext<CR>',
+        { noremap = true, silent = true }
+      )
+      vim.api.nvim_set_keymap(
+        'n',
+        'mp',
+        ':BookmarkPrev<CR>',
+        { noremap = true, silent = true }
+      )
+      vim.api.nvim_set_keymap(
+        'n',
+        'mx',
+        ':BookmarkClearAll<CR>',
+        { noremap = true, silent = true }
+      )
+
+      vim.cmd [[ highlight BookmarkSign ctermbg=NONE ctermfg=160 ]]
+      vim.cmd [[ highlight BookmarkLine ctermbg=194 ctermfg=NONE ]]
     end,
   },
 }
